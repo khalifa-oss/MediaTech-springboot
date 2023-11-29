87013ca1f2206c6ce1455f1ff5d9efecead44c99
khalifa-oss <saidikhalifa22@yahoo.fr>
2023-11-29 10:15:25 +0100

    setup MediatechProject

---
+++ src/main/java/com/sk/mediatechspringboot/MediaTechSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sk.mediatechspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaTechSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaTechSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/mediatechspringboot/MediaTechSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.mediatechspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaTechSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
